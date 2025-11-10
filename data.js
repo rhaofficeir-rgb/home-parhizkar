d8362711f2a1afa9d5ba0e0d0b7eb47b8e900543
RHAOFFICE.IR <rhaoffice.ir@gmail.com>
2025-11-10 22:18:04 +0330

    Add files via upload

---
+++ data.js
@@ -0,0 +1,173 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-36001",
+      "name": "360.01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -0.19215946117256877,
+        "pitch": -0.009545911355413494,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0721318751881963,
+          "pitch": 0.11566591823801531,
+          "rotation": 0,
+          "target": "3-36004"
+        },
+        {
+          "yaw": 1.4913631113055708,
+          "pitch": 0.284834580242757,
+          "rotation": 0,
+          "target": "1-36002"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.226494270195845,
+          "pitch": -0.36496355667521385,
+          "title": "RHAOFFICE",
+          "text": "09128575405"
+        }
+      ]
+    },
+    {
+      "id": "1-36002",
+      "name": "360.02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.0300515244558142,
+          "pitch": -0.3657109258580391,
+          "title": "RHAOFFICE",
+          "text": "09128575405"
+        }
+      ]
+    },
+    {
+      "id": "2-36003",
+      "name": "360.03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 2.2877359549666902,
+        "pitch": 0.05299817619028957,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.2768345672456984,
+          "pitch": -0.33052959146896654,
+          "title": "RHAoffice.ir",
+          "text": "09128575405"
+        }
+      ]
+    },
+    {
+      "id": "3-36004",
+      "name": "360.04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -2.241322723145478,
+        "pitch": 0.0845833376734948,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7122162743393767,
+          "pitch": 0.042292632009102604,
+          "rotation": 0,
+          "target": "1-36002"
+        },
+        {
+          "yaw": 3.121928517610474,
+          "pitch": 0.13962512647781722,
+          "rotation": 0,
+          "target": "0-36001"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0386961872062024,
+          "pitch": -0.30631102119140863,
+          "title": "RHAOFFICE.IR",
+          "text": "09128575405"
+        }
+      ]
+    }
+  ],
+  "name": "HOME PARHIZKAR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
